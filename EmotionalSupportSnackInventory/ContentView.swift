63c17816aa34bb6207d46f7e546efe1a8eaf7ba0
GreenRamSilgen <kiranshrs@yahoo.com>
2026-02-01 12:22:51 -0800

    Added initial app icon and launcscreen

--- EmotionalSupportSnackInventory/ContentView.swift
+++ EmotionalSupportSnackInventory/ContentView.swift
@@ -7,6 +7,35 @@
 
 import SwiftUI
 
+/*
+ Mock Data:
+ 
+ name: "Cheddar Goldfish",
+ onHand: 3,
+ notes: "Best eaten by the handful while doomscrolling.",
+ comfortLevel: 1
+
+ name: "Spicy Takis",
+ onHand: 1,
+ notes: "Left lips numb last time. No regrets.",
+ comfortLevel: 2
+
+ name: "Frozen Thin Mints",
+ onHand: 3,
+ notes: "A classy cold snack. Seasonal, like depression.",
+ comfortLevel: 1
+
+ name: "Trader Joe’s Scandinavian Swimmers",
+ onHand: 2,
+ notes: "Pretends to be healthy. Not fooling anyone.",
+ comfortLevel: 3
+
+ name: "Mom's Cookies",
+ onHand: 1,
+ notes: "Nothing better. Like a warm blanket",
+ comfortLevel: 5
+ 
+ */
 struct ContentView: View {
     var body: some View {
         VStack {
