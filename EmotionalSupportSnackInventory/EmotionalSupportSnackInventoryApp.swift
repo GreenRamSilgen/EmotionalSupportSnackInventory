3baffb748a0131c1ffc1c750ad7b865c5cf4000e
GreenRamSilgen <kiranshrs@yahoo.com>
2026-02-01 12:17:31 -0800

    Initial Commit

---
+++ EmotionalSupportSnackInventory/EmotionalSupportSnackInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionalSupportSnackInventoryApp.swift
+//  EmotionalSupportSnackInventory
+//
+//  Created by Kiran Shrestha on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct EmotionalSupportSnackInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
